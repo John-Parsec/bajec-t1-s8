6c0cb7f8f4670a0bdac174b19dfb73f74b777b7f
João Manoel <jmaoliveira.cic@uesc.br>
2023-10-09 22:11:24 -0300

    feat class Agencia

---
+++ p24/src/Agencia.cpp
@@ -0,0 +1,134 @@
+#include "Turismo.hpp"
+
+Agencia::Agencia(string nome = "", string cnpj = ""){
+    this->nome = nome;
+}
+
+string Agencia::getNome(){
+    return this->nome;
+}
+
+string Agencia::getCnpj(){
+    return this->cnpj;
+}
+
+void Agencia::setNome(string nome){
+    this->nome = nome;
+}
+
+void Agencia::setCnpj(string cnpj){
+    this->cnpj = cnpj;
+}
+
+vector<Cliente*> Agencia::getClientes(){
+    return this->clientes;
+}
+
+vector<Pacote*> Agencia::getPacotes(){
+    return this->pacotes;
+}
+
+vector<Evento*> Agencia::getEventos(){
+    return this->eventos;
+}
+
+vector<Venda*> Agencia::getVendas(){
+    return this->vendas;
+}
+
+void Agencia::addCliente(Cliente* cliente){
+    this->clientes.push_back(cliente);
+}
+
+void Agencia::addPacote(Pacote* pacote){
+    this->pacotes.push_back(pacote);
+}
+
+void Agencia::addEvento(Evento* evento){
+    this->eventos.push_back(evento);
+}
+
+void Agencia::addVenda(Venda* venda){
+    this->vendas.push_back(venda);
+}
+
+bool Agencia::removeCliente(Cliente* cliente){
+    for(auto it = this->clientes.begin(); it != this->clientes.end(); ++it){
+        if(*it == cliente){
+            this->clientes.erase(it);
+            return true;
+        }
+    }
+    return false;
+}
+
+bool Agencia::removePacote(Pacote* pacote){
+    for(auto it = this->pacotes.begin(); it != this->pacotes.end(); ++it){
+        if(*it == pacote){
+            this->pacotes.erase(it);
+            return true;
+        }
+    }
+}
+
+bool Agencia::removeEvento(Evento* evento){
+    for(auto it = this->eventos.begin(); it != this->eventos.end(); ++it){
+        if(*it == evento){
+            this->eventos.erase(it);
+            return true;
+        }
+    }
+}
+
+bool Agencia::removeVenda(Venda* venda){
+    for(auto it = this->vendas.begin(); it != this->vendas.end(); ++it){
+        if(*it == venda){
+            this->vendas.erase(it);
+            return true;
+        }
+    }
+    return false;
+}
+
+void Agencia::cadastarRoteiro(string nome){
+    this->eventos.push_back(new Roteiro(nome));
+}
+
+void Agencia::cadastarDeslocamento(Evento origem, Evento destino){
+    this->eventos.push_back(new Deslocamento(origem, destino));
+}
+
+void Agencia::cadastarPernoite(string nome){
+    this->eventos.push_back(new Pernoite(nome));
+}
+
+void Agencia::criarPacote(string nome, vector<Evento*> eventos){
+    Pacote pacote(nome);
+
+    for(auto it = eventos.begin(); it != eventos.end(); ++it){
+        pacote.addEvento(*it);
+    }    
+
+    this->pacotes.push_back(&pacote);
+}
+
+void Agencia::addCliente(string nome, string cpf){
+    this->clientes.push_back(new Cliente(nome, cpf));
+}
+
+void Agencia::addDependente(string nome, Cliente* dependente_de){
+    Dependete dependente(nome, dependente_de);
+
+    this->dependentes.push_back(&dependente);
+}
+
+void Agencia::addDependente(string nome, int index){
+    Dependete dependente(nome, this->clientes[index]);
+
+    this->dependentes.push_back(&dependente);
+}
+
+void Agencia::addVenda(Cliente* cliente, Pacote* pacote){
+    this->vendas.push_back(new Venda(cliente, pacote));
+}
+
