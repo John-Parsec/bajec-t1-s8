af1ca4cf58aee8aa2a1dd5a96677ce49760b449b
Beatriz-ux <beatrizpereira.ld@gmail.com>
2023-10-13 11:16:05 -0300

    Fix: teste de adionar usuario,livro Fx: teste de emprestimo de livros

--- p21/src/main.cpp
+++ p21/src/main.cpp
@@ -6,9 +6,23 @@
 using namespace std;
 
 int main(void) {
-    time_t tempo;
-    tempo = time(NULL);
-    cout <<"Data:"<< formatarData(tempo) << endl;
+    Livro livro1("Ana Lima", "Pé de figo");
+
+
+    Usuario usuario1("Leticia", "123456789", "123456789");
+    Usuario usuario2("Patricia", "123456789", "123456789");
+    Usuario usuario3("Caio", "123456789", "123456789");
+
+    Biblioteca biblioteca;
+    biblioteca.adicionarLivro(livro1, 2);
+    biblioteca.registrarEmprestimo(livro1, usuario1);
+    biblioteca.registrarEmprestimo(livro1, usuario2);
+    biblioteca.registrarEmprestimo(livro1, usuario3);
+
+
+
+
+
     /*struct tm data = *localtime(&tempo);
     string completa = to_string( data.tm_mday)+"/"+ to_string(data.tm_mon + 1) +"/"+ to_string(data.tm_year + 1900);
     cout << completa << endl;
