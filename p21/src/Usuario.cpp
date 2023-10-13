beca49026c3aac041ecd535b81999e39e8564a6a
Beatriz-ux <beatrizpereira.ld@gmail.com>
2023-10-13 16:06:20 -0300

    Feat: classe usuario

--- p21/src/Usuario.cpp
+++ p21/src/Usuario.cpp
@@ -5,3 +5,23 @@ Usuario :: Usuario (string nome, string cpf, string telefone){
     this->cpf = cpf;
     this->telefone = telefone;
 }
+
+string Usuario :: getNome(){
+    return this->nome;
+}
+
+string Usuario :: getTelefone(){
+    return this->telefone;
+}
+
+string Usuario :: getCPF(){
+    return this->cpf;
+}
+
+void Usuario :: setNome(string nome){
+    this->nome = nome;
+}
+
+void Usuario :: setTelefone(string telefone){
+    this->telefone = telefone;
+}
